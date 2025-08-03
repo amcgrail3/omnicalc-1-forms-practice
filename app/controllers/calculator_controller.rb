a231c82d6667c74d36ab16b32ceebf5c6a9f831f
amcgrail3 <111305582+amcgrail3@users.noreply.github.com>
2025-08-03 19:11:47 +0000

    first calculator working

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -2,4 +2,12 @@ class CalculatorController < ApplicationController
   def homepage_action
     render({ :template => "calculator_templates/homepage" })
   end
+
+  def square
+    @square = params.fetch("square").to_f
+
+    @number_squared_result = @square * @square
+
+    render({ :template => "calculator_templates/square" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculator", :action => "homepage_action" })
+  get("/square",        { :controller => "calculator", :action => "square" })
+  get("/square/new",        { :controller => "calculator", :action => "homepage_action" })
 end
