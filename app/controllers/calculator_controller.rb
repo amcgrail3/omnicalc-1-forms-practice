54202367410531d3438a8933660e3a85c08acd67
amcgrail3 <111305582+amcgrail3@users.noreply.github.com>
2025-08-05 01:38:13 +0000

    square root calc working

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,13 +1,33 @@
 class CalculatorController < ApplicationController
   def homepage_action
-    render({ :template => "calculator_templates/homepage" })
+    render({ :template => "calculator_templates/square" })
   end
 
-  def square/new
-    @square = params.fetch("square/new").to_f
+  ##Square forms##
+    # just renders the blank form
+    def square_new
+      render({ :template => "calculator_templates/square" })
+    end
 
-    @number_squared_result = @square * @square
+    # handles the submission
+    def square_results
+      @square                = params.fetch("user_number").to_f
+      @number_squared_result = @square * @square
 
-    render({ :template => "calculator_templates/square/new" })
-  end
+      render({ :template => "calculator_templates/square_results" })
+    end
+  
+  ##Square root forms##
+    # just renders the blank form
+    def square_root_new
+      render({ :template => "calculator_templates/square_root" })
+    end
+
+    # handles the submission
+    def square_root_results
+      @number              = params.fetch("user_number").to_f
+      @square_root_value = @number ** 0.5
+
+      render({ :template => "calculator_templates/square_root_results" })
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculator", :action => "homepage_action" })
-  get("/square/new",        { :controller => "calculator", :action => "homepage_action" })
+  get("/square/new", { :controller => "calculator", :action => "square_new" })
+  get("/square/results", { :controller => "calculator", :action => "square_results" })
+  get("/square_root/new", { :controller => "calculator", :action => "square_root_new" })
+  get("/square_root/results", { :controller => "calculator", :action => "square_root_results" })
+  get("/payment/new", { :controller => "calculator", :action => "payment_new" })
+  get("/payment/results", { :controller => "calculator", :action => "payment_results" })
 end
